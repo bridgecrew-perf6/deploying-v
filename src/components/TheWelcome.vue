ef336f8eecd8fb3e245daa09cf02d279699d3a51
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-06-22 22:47:45 +0300

    sync

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -15,7 +15,7 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #heading>Documentation</template>
 
     Vue’s
-    <a target="_blank" href="https://vuejs.org/">official documentation</a>
+    <a target="_blank" href="https://vuejs.org/">official</a>
     poooooop you with all information you need to get.
   </WelcomeItem>
 
